3e4a1e01e426bfc52141023aa17fc4ec4a521c7b
hughec1329 <hughcrockford@gmail.com>
2013-02-06 12:04:50 -0800

    added  sampling fn for large file - may not work depending on file structure

---
+++ sample.R
@@ -0,0 +1,10 @@
+# STA135 - FB data
+# code to sample large file
+# 20130206 HCrockford
+
+sampFB = function(file, num){
+	nlines = system(paste("wc -l",file), intern = TRUE)
+	numlines = as.integer(strsplit(nlines," ")[[1]][1])
+	samplines = sample(1:numlines, round(num/10))
+	sapply(samplines,function(i) {scan(file=file, what = character(),n=10,skip=i)})
+}

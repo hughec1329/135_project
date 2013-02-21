48266075b841539dc7165d4c0f4fd6490b223147
hughec1329 <hughcrockford@gmail.com>
2013-02-21 08:37:55 -0800

    further work on nets, got max, need to work out how to structure multi net - list? how querey

--- networks.R
+++ networks.R
@@ -6,8 +6,18 @@ props = read.delim("~/data/props.txt", sep = "#",header = FALSE)
 names(props) = c("uid", "#samp", "totFR", "priv", "NID")
 table(props$NID)
 # some beloing to multiple networks.
-popularNets = sort(table(props$NID), decreasing = TRUE ) 	# find popular nets.
-head(popularNets)
-strsplit(props$NID,"(?=[\\Q|\\E])",perl = TRUE)
+popularNets = sort(table(props$NID), decreasing = TRUE ) 	# find popular nets. most popular is No network (~450k)
+popularNets = popularNets[-1]	# cull no nets
+head(popularNets,n=100)	
+sum(popularNets[1:100])	# first 100 most popular nets acocunt for 230k peeps.
+
+
+max(sapply(strsplit(as.character(props$NID),"|",fixed = TRUE),length))	# get amx number of nets anyone belongs to. = 5
+
+# how to structure - in list? 
+## need to be able to sort by network - match on all 5 dimensions?
+## how test if my second net is same as evans first?
+
+
 
 

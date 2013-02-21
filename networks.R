911396eba810e1a5163a6663a03aa3902a511bc0
hughec1329 <hughcrockford@gmail.com>
2013-02-21 14:58:01 -0800

    got NID split working, attempt at throwing in list to compare

--- networks.R
+++ networks.R
@@ -9,7 +9,10 @@ table(props$NID)
 popularNets = sort(table(props$NID), decreasing = TRUE ) 	# find popular nets. most popular is No network (~450k)
 popularNets = popularNets[-1]	# cull no nets
 head(popularNets,n=100)	
+plot(head(popularNets,n=100))
 sum(popularNets[1:100])	# first 100 most popular nets acocunt for 230k peeps.
+sum(popularNets[1:20])	# first 20 most popular nets acocunt for 110k peeps.
+sum(popularNets[1:20])/sum(popularNets)		# top 20 nets cover 20% of popn
 
 
 max(sapply(strsplit(as.character(props$NID),"|",fixed = TRUE),length))	# get amx number of nets anyone belongs to. = 5
@@ -18,6 +21,11 @@ max(sapply(strsplit(as.character(props$NID),"|",fixed = TRUE),length))	# get amx
 ## need to be able to sort by network - match on all 5 dimensions?
 ## how test if my second net is same as evans first?
 
+try = lapply(props, function(i) c(i[-5],strsplit(as.character(i[5]),"|",fixed = TRUE)))
+
+
+u = data.frame(id = c(1,2,3,4,5), NID = c("23","23|2343","3434","342","343|344"))
+o = apply(u,1,function(i) c(i[1],strsplit(as.character(i[2]),"|",fixed = TRUE)))
 
 
 

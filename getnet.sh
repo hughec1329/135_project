e35cc067ace8954102ecb6ca26898a4cfb10052b
hughec1329 <hughcrockford@gmail.com>
2013-03-12 10:10:54 -0700

    got getnet working - using sed heaps faster, worked out only match on actual net, 69 not 269

--- getnet.sh
+++ getnet.sh
@@ -4,11 +4,13 @@
 # $1 - filename
 # $2 - network to search for
 
-lnumber=$(cat $1 | cut -d "#" -f 5 | grep -nr $2 | cut -d ":" -f 1)	# find line numbers of net match. ( could do simple grep but will get user ids with net name included.)
+lnumber=$(cat ~/data/mhrw-nodeproperties-anonymized.txt | cut -d "#" -f 5 | tr "|" " " | grep -nr "^"$2 | cut -d ":" -f 1)	# find line numbers of net match. ( could do simple grep but will get user ids with net name included.)
+
 for i in $lnumber
 do
-	awk 'NR=='$i'{print $0}' $1 >> 'net_'$2.txt	# return that line
-	# echo 'NR=='$i'{print $0}' $1
+	# awk 'NR=='$i'{print $0}' $1 >> 'net_'$2.txt	# return that line
+	 # head -n $i $1 | tail -n 1 # >> 'net_'$2.txt	# much faster
+	 sed $i'q;d' $1 >> 'net_'$2.txt
 done
 
 	

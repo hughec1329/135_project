08692cc3669ae3070f664bf602e0a4fe101f7ade
hughec1329 <hughcrockford@gmail.com>
2013-03-12 10:20:50 -0700

    stripnet working - wont start until tonight as will take loooong time

--- stripnet.sh
+++ stripnet.sh
@@ -9,6 +9,6 @@ nets=$(cat ~/data/topnets | awk -F " " '{print $2}' | head -n 100)
 
 for t in $nets
 do
-	# ./getnet.sh ~/data/fb_mhrw.txt $i > net_$t.txt
-	echo "foo" >> "~/data/stripped/net_"$t".txt"
+	./getnet.sh ~/data/fb_mhrw.txt $t >> "/home/hugh/data/stripped/net_"$t".txt"
+	echo "done " $t 
 done

ce2b8ad7b777acc4afa6de6603a54fe188d87e5a
hughec1329 <hughcrockford@gmail.com>
2013-03-13 21:53:07 -0700

    got stripper working - connection to monet, fast strip, and return values we need. nothing kept in memory. need evans edgelist maker to throw into eliots var calculator

--- getnum.sh
+++ getnum.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-nets=$(cat ./topnets | awk -F " " '{print $2}' | head -n 100)
-
-for i in $nets
-do
-	cat ~/data/mhrw-nodeproperties-anonymized.txt | cut -d "#" -f 5 | awk -F "|" '{for(i=1;i<=NF;i++) if ($i =='$i') print NR}' >> '/home/hugh/data/list/l_'$i
-	echo $i
-done
-
+cat ~/data/mhrw-nodeproperties-anonymized.txt | cut -d "#" -f 5 | awk -F "|" '{for(i=1;i<=NF;i++) if ($i =='$1') print NR}'
 
--- stripper.R
+++ stripper.R
@@ -6,9 +6,46 @@ library(MonetDB.R)
 library(RMonetDB)
 "SELECT * FROM sys.fb WHERE no IN (" t ")" 
 
-con = dbConnect(MonetDB.R(),"monetdb://localhost/fb")
+con = dbConnect(MonetDB.R(),"monetdb://localhost/fb",timeout = 99999)
 dbListTables(con)
 
 dbGetQuery(con, "SELECT * FROM fb WHERE no = 3")
+
+
+net = 291
+lin = as.integer(system(paste("./justgetnum.sh", net),intern=TRUE))
+lines = paste(lin,collapse=",")
+arg = sprintf("SELECT * FROM fb WHERE no IN (%s);",lines)
+it = dbGetQuery(con, arg)		# WORKING?
+
+net = c(112,234)
+ret = list()
+
+retun = for(i in net){		# not working in loop?? - returns nothing.
+	lin = as.integer(system(paste("./justgetnum.sh", i),intern=TRUE))
+	lines = paste(lin,collapse=",")
+	arg = sprintf("SELECT * FROM fb WHERE no IN (%s);",lines)
+	return(dbGetQuery(con, arg))		# WORKING?
+}
+
+
+
+
+
+
+dbGetQuery(con, "SELECT * FROM fb WHERE no IN (32,424,6454);")	# works fine
+fin = dbSendQuery(con, arg)
+fetch(fin)	# cant fetch monet?
+
+monet.frame(con, arg)
+
+# works when I write to file then read in?? 
+write(arg,"arg.txt")
+system("mclient -d fb < arg.txt", intern = TRUE)
+
+nom = system(paste("./dbget.sh", net),intern = TRUE)
+system(paste("mclient -d fb < ",nom) , intern = TRUE)
+
+
 
 

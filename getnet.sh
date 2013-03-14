67ffa03cbac3d45ebd795cd3ff383e4509db88f9
hughec1329 <hughcrockford@gmail.com>
2013-03-13 17:07:27 -0700

    got getnet working properly

--- getnet.sh
+++ getnet.sh
@@ -4,7 +4,11 @@
 # $1 - filename
 # $2 - network to search for
 
-lnumber=$(cat ~/data/mhrw-nodeproperties-anonymized.txt | cut -d "#" -f 5 | tr "|" " " | grep -nr "^"$2 | cut -d ":" -f 1)	# find line numbers of net match. ( could do simple grep but will get user ids with net name included.)
+# lnumber=$(cat ~/data/mhrw-nodeproperties-anonymized.txt | cut -d "#" -f 5 | tr "|" " " | grep -nr "^"$2 | cut -d ":" -f 1)	# find line numbers of net match. ( could do simple grep but will get user ids with net name included.)
+
+
+lnumber=$(cat ~/data/mhrw-nodeproperties-anonymized.txt | cut -d "#" -f 5 | awk -F "|" '{for(i=1;i<=NF;i++) if ($i =='$2') print NR}')
+
 
 # STILL MISSING IF SECOND NET - need to work on grep statement to allow anywhere but only if preceed by space OR at start
 

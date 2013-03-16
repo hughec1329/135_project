cd8db468f8436b09b72e135afef281d412458bb6
hughec1329 <hughcrockford@gmail.com>
2013-03-16 11:05:14 -0700

    added nedge and npeeps to spit,  going to run on important nets

--- stripper.R
+++ stripper.R
@@ -18,22 +18,23 @@ spit = 	function(i){
 	it = dbGetQuery(con, arg)
 	out = data.frame(do.call("rbind",apply(it,1,makeedge)))
 	outt = graph.data.frame(out,directed = FALSE)
+	npeeps = length(it[,1])
 	apl.noloner = average.path.length(outt,directed=F)
 	apl.loner = average.path.length(outt,directed=F,unconnected=F)
-		nedge = sum(degree(outt))
+	nedge = sum(degree(outt))
 	nnode = length(V(outt))		
 	ncluster = clusters(outt)$no		
 	trans = transitivity(outt, type="global")
 	diam = diameter(outt)
 	dens = graph.density(outt)
-	c(apl.noloner,apl.loner,nnode,ncluster,trans,diam,dens)
+	c(npeeps,apl.noloner,apl.loner,nedge,nnode,ncluster,trans,diam,dens)
 }
 
 
 grab = function(i){
 	ret = data.frame(t(sapply(i, spit)))
 	row.names(ret) = i
-	names(ret) = c("apl.noloner","apl.loner","nnode","ncluster","trans","diam","dens")
+	names(ret) = c("neeps","apl.noloner","apl.loner","nedge","nnode","ncluster","trans","diam","dens")
 	return(ret)
 }
 
@@ -54,6 +55,6 @@ topnet[topnet[,1] %in% seq(500,50,-50),]
 int = c(1,2,4,5,6,9,13,15,18,23,30,35,48,69,93,118,167,208,239,273,309,376,471,609,959)	
 ournet = topnet[int,2]	# 25 nets spread throughout size range.
 
-fbak(grab(ournet))
+fbak = grab(ournet)
 
 
